5ab3300a14fca98c7c2741d84b95b0077e6abfa1
w-jiwonchoi <w@yonsei.ac.kr>
2025-10-16 04:32:49 +0000

    revised

--- hqrnn/utils/seed.py
+++ hqrnn/utils/seed.py
@@ -3,6 +3,10 @@ from jax import random
 
 _main_key = random.PRNGKey(42)
 
+def set_seed(seed: int):
+    global _main_key
+    _main_key = random.PRNGKey(seed)
+
 def get_key():
     global _main_key
     _main_key, subkey = random.split(_main_key)
